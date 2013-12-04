425195195965daa353f87d6a45a23a88fdc290c7
Judah Holanda Correia Lima <Judah@Mac.local>
2013-12-04 16:21:50 -0300

    first

---
+++ IOSGas/IOSGas/ADSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  ADSAppDelegate.h
+//  IOSGas
+//
+//  Created by Judah Holanda on 12/4/13.
+//  Copyright (c) 2013 Judah Holanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ IOSGas/IOSGas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOSGas
+//
+//  Created by Judah Holanda on 12/4/13.
+//  Copyright (c) 2013 Judah Holanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADSAppDelegate class]));
+    }
+}
