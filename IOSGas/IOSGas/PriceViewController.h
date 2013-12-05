931423d9c528b5a16c9f56e5460dfa3b16b0c183
Tarciso Torres <tarcisotorres@Tarcisos-Mac.local>
2013-12-04 23:31:49 -0300

    Criacao da classe Price

---
+++ IOSGas/IOSGas/PriceViewController.h
@@ -0,0 +1,13 @@
+//
+//  PriceViewController.h
+//  IOSGas
+//
+//  Created by Tarciso Torres on 12/4/13.
+//  Copyright (c) 2013 Judah Holanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PriceViewController : UIViewController
+
+@end
---
+++ IOSGas/IOSGas/PriceViewController.m
@@ -0,0 +1,38 @@
+//
+//  PriceViewController.m
+//  IOSGas
+//
+//  Created by Tarciso Torres on 12/4/13.
+//  Copyright (c) 2013 Judah Holanda. All rights reserved.
+//
+
+#import "PriceViewController.h"
+
+@interface PriceViewController ()
+
+@end
+
+@implementation PriceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	self.title = @"Price";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
