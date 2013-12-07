fbb869d47d8c8ac6b6768c6a4e46abe4fa8244b1
Judah Holanda Correia Lima <Judah@mac.home>
2013-12-07 16:54:33 -0300

    update

--- IOSGas/IOSGas/ADSAppDelegate.m
+++ IOSGas/IOSGas/ADSAppDelegate.m
@@ -105,6 +105,8 @@
     
     NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"IOSGas.sqlite"];
     
+    NSLog(@"%@", storeURL);
+    
     NSError *error = nil;
     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
     if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
